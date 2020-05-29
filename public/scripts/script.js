4e1daeddc68f9e745b5c53248954303dd6389ba2
rohanC-dev <rohan.chopra241@gmail.com>
2020-05-29 14:07:38 -0400

    changed backgrounds for different pages

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,11 @@
-// var width = $("#moviePoster").clientWidth;
-// var height = $("#moviePoster").clientHeight;
-
-// console.log($("#moviePoster"));
-
-// //$(".jumbotron").style.padding-bottom;
-
 $(document).ready(function () {
     var height = $("#moviePoster").height();
     height = height + 64;
     $("#topJumbotron").css("height", height);
+
+    $("#mainContainer").parent().css("background", "#cfcfcf");
 });
+
+
+
 
